1d17a40b417b2a3b381effcb7660f685faacf2fb
hovo-hakobyan <88614889+hovo-hakobyan@users.noreply.github.com>
2023-04-07 14:08:17 +0200

    2 paddles + movement

--- OverlordProject/Scenes/Week 3/PongScene.cpp
+++ OverlordProject/Scenes/Week 3/PongScene.cpp
@@ -15,7 +15,6 @@ void PongScene::Initialize()
 	//Materials
 	auto pBouncyMaterial = physX.createMaterial(.5f, .5f, 1.f);
 
-
 	//Set a new fixed camera
 	auto prevCamera = m_SceneContext.pCamera;
 	const auto pFixedCamera = new FixedCamera();
@@ -27,22 +26,65 @@ void PongScene::Initialize()
 	RemoveChild(prevCamera->GetGameObject(),true);
 
 	//Init paddles
-	const XMFLOAT3 paddleSize{ 1.f,5.f,3.f };
-	XMFLOAT3 paddleStart{ -5.f,0.f,0.f };
+	const XMFLOAT3 paddleSize{ 1.f,6.f,2.5f };
+	XMFLOAT3 paddleStart{ -20.f,0.f,0.f };
 	PxBoxGeometry paddleGeo{ paddleSize.x / 2.f,paddleSize.y / 2.f, paddleSize.z / 2.f };
 
 	//Left paddle
 	m_pLeftPaddle = new CubePrefab(paddleSize, XMFLOAT4{ Colors::Red });
 	AddChild(m_pLeftPaddle);
 	m_pLeftPaddle->GetTransform()->Translate(paddleStart);
-	auto pRigidBody = m_pLeftPaddle->AddComponent(new RigidBodyComponent());
-	pRigidBody->AddCollider(paddleGeo, *pBouncyMaterial);
-	pRigidBody->SetKinematic(true);
+	m_pLeftRigidBody = m_pLeftPaddle->AddComponent(new RigidBodyComponent());
+	m_pLeftRigidBody->AddCollider(paddleGeo, *pBouncyMaterial);
+	m_pLeftRigidBody->SetKinematic(true);
+
+	//Right paddle
+	m_pRightPaddle = new CubePrefab(paddleSize, XMFLOAT4{ Colors::Green });
+	AddChild(m_pRightPaddle);
+	m_pRightPaddle->GetTransform()->Translate(-paddleStart.x, paddleStart.y, paddleStart.z);
+	m_pRightRigidBody = m_pRightPaddle->AddComponent(new RigidBodyComponent());
+	m_pRightRigidBody->AddCollider(paddleGeo, *pBouncyMaterial);
+	m_pRightRigidBody->SetKinematic(true);
+
+	//Input
+	auto input = m_SceneContext.pInput;
+
+	input->AddInputAction(InputAction{ InputIds::FirstUp,InputState::down,'W'});
+	input->AddInputAction(InputAction{ InputIds::FirstDown,InputState::down,'S' });
+	input->AddInputAction(InputAction{ InputIds::SecondUp,InputState::down,VK_UP });
+	input->AddInputAction(InputAction{ InputIds::SecondDown,InputState::down,VK_DOWN });
+
 }
 
 void PongScene::Update()
 {
+	const auto input = m_SceneContext.pInput;
+	float deltaTime = m_SceneContext.pGameTime->GetElapsed();
 
+	if (input->IsActionTriggered(InputIds::FirstUp))
+	{
+		XMFLOAT3 currentPos{ m_pLeftRigidBody->GetPosition() };
+		XMFLOAT3 destination{currentPos.x,currentPos.y + m_PaddleSpeed * deltaTime,currentPos.z};
+		m_pLeftRigidBody->GetTransform()->Translate(destination);
+	}
+	if (input->IsActionTriggered(InputIds::FirstDown))
+	{
+		XMFLOAT3 currentPos{ m_pLeftRigidBody->GetPosition() };
+		XMFLOAT3 destination{ currentPos.x,currentPos.y - m_PaddleSpeed * deltaTime,currentPos.z };
+		m_pLeftRigidBody->GetTransform()->Translate(destination);
+	}
+	if (input->IsActionTriggered(InputIds::SecondDown))
+	{
+		XMFLOAT3 currentPos{ m_pRightRigidBody->GetPosition() };
+		XMFLOAT3 destination{ currentPos.x,currentPos.y - m_PaddleSpeed * deltaTime,currentPos.z };
+		m_pRightRigidBody->GetTransform()->Translate(destination);
+	}
+	if (input->IsActionTriggered(InputIds::SecondUp))
+	{
+		XMFLOAT3 currentPos{ m_pRightRigidBody->GetPosition() };
+		XMFLOAT3 destination{ currentPos.x,currentPos.y + m_PaddleSpeed * deltaTime,currentPos.z };
+		m_pRightRigidBody->GetTransform()->Translate(destination);
+	}
 }
 
 void PongScene::Draw()
--- OverlordProject/Scenes/Week 3/PongScene.h
+++ OverlordProject/Scenes/Week 3/PongScene.h
@@ -17,7 +17,21 @@ protected:
 	void OnGUI() override;
 
 private:
+	enum InputIds
+	{
+		FirstUp,
+		FirstDown,
+		SecondUp,
+		SecondDown
+	};
+
+	float m_PaddleSpeed{ 10.f };
+
 	GameObject* m_pLeftPaddle{};
+	GameObject* m_pRightPaddle{};
+
+	RigidBodyComponent* m_pLeftRigidBody{};
+	RigidBodyComponent* m_pRightRigidBody{};
 };
 
 
