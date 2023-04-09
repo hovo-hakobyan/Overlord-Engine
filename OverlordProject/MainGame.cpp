d7d50c9b7b449e45229010608452d71647c7247d
hovo-hakobyan <88614889+hovo-hakobyan@users.noreply.github.com>
2023-04-09 14:02:27 +0200

    uber material scene init

--- OverlordProject/MainGame.cpp
+++ OverlordProject/MainGame.cpp
@@ -25,7 +25,7 @@
 
 #ifdef W4
 #include "Scenes/Week 4/ModelTestScene.h"
-//#include "Scenes/Week 4/UberMaterialScene.h"
+#include "Scenes/Week 4/UberMaterialScene.h"
 //#include "Scenes/Week 4/SpikyScene.h"
 //#include "Scenes/Week 4/SpriteTestScene.h"
 #endif
@@ -102,8 +102,8 @@ void MainGame::Initialize()
 #endif
 
 #ifdef W4
-	SceneManager::Get()->AddGameScene(new ModelTestScene());
-	//SceneManager::Get()->AddGameScene(new UberMaterialScene());
+	//SceneManager::Get()->AddGameScene(new ModelTestScene());
+	SceneManager::Get()->AddGameScene(new UberMaterialScene());
 	//SceneManager::Get()->AddGameScene(new SpikyScene());
 	//SceneManager::Get()->AddGameScene(new SpriteTestScene());
 #endif
